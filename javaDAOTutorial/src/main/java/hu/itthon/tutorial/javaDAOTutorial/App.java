589537ead2e26494bca9518d973538dc524c11c4
Balogh Istvan <istvan747@gmail.com>
2018-09-20 15:37:38 +0200

    JavaDAOTutorial project is created.

---
+++ javaDAOTutorial/src/main/java/hu/itthon/tutorial/javaDAOTutorial/App.java
@@ -0,0 +1,13 @@
+package hu.itthon.tutorial.javaDAOTutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
